c9074226dfb03d016637a8526a7de50515ac3f45
Veronika <sherr.veronika@gmail.com>
2024-02-26 18:47:46 -0500

    add some tests

--- test_pipline_data_core.py
+++ test_pipline_data_core.py
@@ -3,10 +3,35 @@ import numpy as np
 from numpy import nan
 import pandas as pd
 
-df = pd.read_excel(r'.\Product.xlsx')
+
+@pytest.fixture()
+def df():
+    df = pd.read_excel(r'.\Product.xlsx')
+    return df
 
 
 # check if column exist
-def test_col_exist():
+def test_col_exist(df):
     name = "ProductKey"
     assert name in df.columns
+    name = "EnglishProductName"
+    assert name in df.columns
+
+
+# check for nulls
+def test_null(df):
+    assert np.where(df['ProductKey'].isnull())
+
+
+# check values and unique
+def test_unique_val(df):
+    assert pd.Series(df['ProductKey']).is_unique
+
+
+# check data type
+def test_productkey_isint(df):
+    assert (df['ProductKey'].dtype == int or df['ProductKey'].dtype == np.int64)
+
+
+def test_productname_dtype_str(df):
+    assert (df['EnglishProductName'].dtype == str or df['EnglishProductName'].dtype == 'O')
